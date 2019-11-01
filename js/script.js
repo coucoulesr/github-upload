f17d42c37de3fb35e38a53bd83d09c4ebc01b5fc
Richard Coucoules <richardcoucoules@gmail.com>
2019-10-31 20:41:45 -0700

    Adds button functionality with appropriate styling and support for French, Spanish, and Italian. Also updates font families.

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const CONTAINER = document.querySelector(".btn-container");
+const TEXTELEMENT = document.querySelector(".main-text");
+const TRANSLATIONS = {
+  "en" : ["English", "French", "Spanish", "Italian"],
+  "fr" : ["Anglais", "Français", "Espagnol", "Italien"],
+  "es" : ["Inglés", "Francés", "Español", "Italiano"],
+  "it" : ["Inglese", "Francese", "Spagnolo", "Italiano"]
+};
+
+var buttons = [];
+
+function addButton(parent, text, classArray = []) {
+  let button = document.createElement("button");
+  button.setAttribute("type", "button");
+  classArray.push("btn");
+  button.setAttribute("class", classArray.join(" "));
+  button.innerHTML = text;
+  parent.appendChild(button);
+  buttons.push(button);
+  return button;
+}
+
+function changeButtons(buttonArray, lang) {
+  for (i = 0; i < buttonArray.length; i++) {
+    buttonArray[i].innerHTML = TRANSLATIONS[lang][i];
+  }
+}
+
+let en = addButton(CONTAINER, "English", ["en"]);
+let fr = addButton(CONTAINER, "French", ["fr"]);
+let es = addButton(CONTAINER, "Spanish", ["es"]);
+let it = addButton(CONTAINER, "Italian", ["it"]);
+
+en.addEventListener("click", () => { TEXTELEMENT.innerHTML = "Hello, world!"; changeButtons(buttons, "en")} );
+fr.addEventListener("click", () => { TEXTELEMENT.innerHTML = "Bonjour, le monde!"; changeButtons(buttons, "fr")} );
+es.addEventListener("click", () => { TEXTELEMENT.innerHTML = "¡Hola, mundo!"; changeButtons(buttons, "es")} );
+it.addEventListener("click", () => { TEXTELEMENT.innerHTML = "Ciao mondo!"; changeButtons(buttons, "it")} );
